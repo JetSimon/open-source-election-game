bd2a573e1c3a9e5e0653834379793d859423d0b2
Jet Simon <jetjdsimon@gmail.com>
2024-12-26 20:29:38 -0800

    use hash router

--- open-source-election-game/src/App.tsx
+++ open-source-election-game/src/App.tsx
@@ -4,7 +4,6 @@ import Game from "./Game";
 import OsegEditor from "./editor/OsegEditor";
 
 import {
-  HashRouter as Router,
   Routes,
   Route,
   Link
@@ -13,28 +12,26 @@ import {
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <div className="HeaderNav">
           <h2 className="Logo">
             OSEG
           </h2>
           <div>
-            <Link to="/open-source-election-game/">Game</Link>
+            <Link to="/">Game</Link>
           </div>
           <div>
-            <Link to="/open-source-election-game/editor">Editor</Link>
+            <Link to="/editor">Editor</Link>
           </div>
         </div>
 
         <Routes>
-          <Route path="/open-source-election-game/" element={<Game></Game>}>
+          <Route path="/" element={<Game></Game>}>
           </Route>
-          <Route path="/open-source-election-game/editor" element={<OsegEditor></OsegEditor>}>
+          <Route path="/editor" element={<OsegEditor></OsegEditor>}>
           </Route>
         </Routes>
       </div>
-    </Router>
   )
 }
 
--- open-source-election-game/src/main.tsx
+++ open-source-election-game/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
