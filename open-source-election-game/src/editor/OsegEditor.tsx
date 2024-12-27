4a910b4f3a4333b9d417a454068040afe461e920
Jet Simon <jetjdsimon@gmail.com>
2024-12-27 15:12:59 -0800

    Test in editor

--- open-source-election-game/src/editor/OsegEditor.tsx
+++ open-source-election-game/src/editor/OsegEditor.tsx
@@ -4,6 +4,7 @@ import "./OsegEditor.css";
 import OsegLeftPanel from "./OsegLeftPanel";
 import OsegRightPanel from "./OsegRightPanel";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import Game from "../Game";
 
 function downloadString(s : string, fileName : string) {
     const element = document.createElement('a');
@@ -25,6 +26,8 @@ function OsegEditor() {
 
     const [logic, setLogic] = useState<string>("");
     const [mapUrl, setMapUrl] = useState<string>("");
+
+    const [isPlaying, setIsPlaying] = useState(false);
 
     async function loadDefaultData() {
         const defaultData = await fetch("./editor/templates/default/data.json");
@@ -50,9 +53,19 @@ function OsegEditor() {
         return <h1>Loading default data, map</h1>;
     }
 
+    if(isPlaying) {
+        return (
+            <div>
+                <button className="RedButton" onClick={() => setIsPlaying(false)}>Stop Playing</button>
+                <Game injectedData={data} injectedLogic={logic} injectedMapUrl={mapUrl}></Game>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h2>OSEG Editor</h2>
+            <button className="GreenButton" onClick={() => setIsPlaying(true)}>Start Playing</button>
             <PanelGroup direction="horizontal" id="group">
             <Panel className="Panel" id="left-panel">
                 <OsegLeftPanel mapUrl={mapUrl} setMapUrl={setMapUrl} setData={setData} setLogic={setLogic} setDataString={setDataString} dataString={dataString} logic={logic}></OsegLeftPanel>
