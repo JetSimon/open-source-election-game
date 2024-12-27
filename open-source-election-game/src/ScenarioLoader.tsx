8a183d2827e65a0440e799b15aec685392d47972
Jet Simon <jetjdsimon@gmail.com>
2024-12-27 15:28:12 -0800

    Add ability to paste custom scenario

--- open-source-election-game/src/ScenarioLoader.tsx
+++ open-source-election-game/src/ScenarioLoader.tsx
@@ -1,6 +1,7 @@
 import ScenarioModel from "./models/ScenarioModel";
 import { useEffect, useState } from "react";
 import Game from "./Game";
+import "./ScenarioLoader.css";
 
 const scenarioNames = ["TestScenario"];
 
@@ -10,9 +11,11 @@ function ScenarioLoader() {
     const [data, setData] = useState<ScenarioModel | null>(null);
     const [logic, setLogic] = useState<string>("");
     const [mapUrl, setMapUrl] = useState<string>("");
+
+    const [dataString, setDataString] = useState<string>("");
+    const [loadingCustomScenario, setLoadingCustomScenario] = useState(false);
   
     useEffect(() => {
-    
       if(currentModName == "") {
         return;
       }
@@ -32,11 +35,38 @@ function ScenarioLoader() {
       loadScenarioFromUrl(currentModName);
     }, [currentModName]);
 
-    if(currentModName == "") {
+    function loadCustomScenario() {
+        try {
+            const customData = JSON.parse(dataString) as ScenarioModel;
+            setData(customData);
+            setLogic(logic);
+            setMapUrl(mapUrl);
+            setLoadingCustomScenario(true);
+        }
+        catch(e) {
+            alert("Error: " + e);
+        }
+    }
+
+    if(currentModName == "" && !loadingCustomScenario) {
         return (
             <div>
                 <h2>Pick a Scenario From These Buttons</h2>
                 {scenarioNames.map((scenarioName) => <button onClick={() => setCurrentModName(scenarioName)}>{scenarioName}</button>)}
+                <h2>Or Paste a Custom Scenario</h2>
+
+                <div className="CustomArea">
+                    <label htmlFor="data">data.json</label>
+                    <textarea rows={8} onChange={(e) => setDataString(e.target.value)} value={dataString} id="data"></textarea>
+
+                    <label htmlFor="logic">logic.js</label>
+                    <textarea rows={8}  onChange={(e) => setLogic(e.target.value)} value={logic} id="logic"></textarea>
+
+                    <label htmlFor="map">map.svg url</label>
+                    <input type="text" onChange={(e) => setMapUrl(e.target.value)} value={mapUrl} id="map"></input>
+
+                    <button onClick={loadCustomScenario} className="GreenButton">Go</button>
+                </div>
             </div>
         )
         }
