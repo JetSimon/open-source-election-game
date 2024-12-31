df11997127df002716e2d62db51b49b98ee22fda
Jet Simon <jetjdsimon@gmail.com>
2024-12-31 11:32:26 -0800

    Better mobile header

--- open-source-election-game/src/App.tsx
+++ open-source-election-game/src/App.tsx
@@ -18,13 +18,13 @@ function App() {
             OSEG
           </a>
           <div>
+            <Link to="/open-source-election-game/about">About</Link>
+          </div>
+          <div>
             <Link to="/open-source-election-game/">Game</Link>
           </div>
           <div>
             <Link to="/open-source-election-game/editor">Editor</Link>
-          </div>
-          <div>
-            <Link to="/open-source-election-game/about">About</Link>
           </div>
           <div>
             <a href="/open-source-election-game/doc">Doc</a>
