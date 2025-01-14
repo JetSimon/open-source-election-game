2c230e61e6c66f218bbf4ffc8caac5bf22054cda
Jet Simon <jetjdsimon@gmail.com>
2025-01-14 15:35:28 -0800

    Add overall margins button

--- open-source-election-game/src/oseg/game/components/StatePoll.tsx
+++ open-source-election-game/src/oseg/game/components/StatePoll.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import StateController from "../../engine/controllers/StateController";
 import { Engine } from "../../engine/Engine";
 import ThemeModel from "../../engine/models/ThemeModel";
@@ -6,50 +7,116 @@ import "./Tooltip.css";
 interface StatePollProps {
   engine: Engine | null;
   stateController: StateController | null;
-  theme : ThemeModel;
+  theme: ThemeModel;
 }
-
 const numberFormatter = Intl.NumberFormat();
 
 function StatePoll(props: StatePollProps) {
   const { stateController, engine, theme } = props;
 
+  const [ showingStateEstimate, setShowingStateEstimate ] = useState(true);
+
   if (engine == null || stateController == null) {
-    return <div className="StatePoll" style={{backgroundColor:theme.primaryGameWindowColor, color:theme.primaryGameWindowTextColor}}>Hover over a state see polling</div>;
+    return <div className="StatePoll" style={{ backgroundColor: theme.primaryGameWindowColor, color: theme.primaryGameWindowTextColor }}>Hover over a state see polling</div>;
+  }
+
+  
+  function evEstimate(engine : Engine) {
+    const estimate = engine.getFinalResults();
+
+    if(estimate.totalElectoralVotes == 0) {
+      return <></>
+    }
+
+    return <div className="StateIssues StatePollValueList">{Array.from(estimate.electoralVotes)
+      .sort((a, b) => b[1] - a[1])
+      .map(([candidateId, evs]) => {
+        const candidate = engine.getCandidateControllerByCandidateId(candidateId);
+
+        if (candidate == null || candidate == undefined) {
+          return (<p>Candidate undefined</p>);
+        }
+
+        const color = candidate.model.color;
+        return (
+          <div className="StatePollValue" key={candidate.model.id}>
+            <div
+              style={{ backgroundColor: color }}
+              className="PollDot"
+            ></div>
+            <span style={{ fontWeight: "bold" }}>
+              {candidate.model.lastName}
+            </span>{" "}
+            - {evs} EVs
+          </div>
+        );
+      })}</div>
+  }
+
+  function pvEstimate(engine : Engine) {
+    const estimate = engine.getFinalResults();
+
+    return Array.from(estimate.popularVotes)
+      .sort((a, b) => b[1] - a[1])
+      .map(([candidateId, opinion]) => {
+        const candidate = engine.getCandidateControllerByCandidateId(candidateId);
+
+        if (candidate == null || candidate == undefined) {
+          return (<p>Candidate undefined</p>);
+        }
+
+        const color = candidate.model.color;
+        return (
+          <div className="StatePollValue" key={candidate.model.id}>
+            <div
+              style={{ backgroundColor: color }}
+              className="PollDot"
+            ></div>
+            <span style={{ fontWeight: "bold" }}>
+              {candidate.model.lastName}
+            </span>{" "}
+            - {(opinion / estimate.totalPopularVotes * 100).toFixed(2)}%
+          </div>
+        );
+      });
+  }
+
+  function stateEstimate(stateController: StateController, engine: Engine) {
+    return Array.from(stateController.opinions)
+      .sort((a, b) => b[1] - a[1])
+      .map(([candidateId, opinion]) => {
+        const candidate = engine.getCandidateControllerByCandidateId(candidateId);
+
+        if (candidate == null || candidate == undefined) {
+          return (<p>Candidate undefined</p>);
+        }
+
+        const color = candidate.model.color;
+        return (
+          <div className="StatePollValue" key={candidate.model.id}>
+            <div
+              style={{ backgroundColor: color }}
+              className="PollDot"
+            ></div>
+            <span style={{ fontWeight: "bold" }}>
+              {candidate.model.lastName}
+            </span>{" "}
+            - {(opinion * 100).toFixed(2)}%
+          </div>
+        );
+      });
   }
 
   return (
-    <div className="StatePoll" style={{backgroundColor:theme.primaryGameWindowColor, color:theme.primaryGameWindowTextColor}}>
+    <div className="StatePoll" style={{ backgroundColor: theme.primaryGameWindowColor, color: theme.primaryGameWindowTextColor }}>
       <h2>{stateController.model.name}</h2>
-      <h3>Polls</h3>
+      <h3>{showingStateEstimate ? "State Polls" : "Overall Polls"} </h3>
       <div className="StatePollValueList">
-        {Array.from(stateController.opinions)
-          .sort((a, b) => b[1] - a[1])
-          .map(([candidateId, opinion]) => {
-            const candidate = engine.getCandidateControllerByCandidateId(candidateId);
-
-            if(candidate == null || candidate == undefined) {
-              return (<p>Candidate undefined</p>)
-            } 
-
-            const color = candidate.model.color;
-            return (
-              <div className="StatePollValue" key={candidate.model.id}>
-                <div
-                  style={{ backgroundColor: color }}
-                  className="PollDot"
-                ></div>
-                <span style={{ fontWeight: "bold" }}>
-                  {candidate.model.lastName}
-                </span>{" "}
-                - {(opinion * 100).toFixed(2)}%
-              </div>
-            );
-          })}
+        {showingStateEstimate ? stateEstimate(stateController, engine) : pvEstimate(engine)}
       </div>
-      <h3>Issue Stances</h3>
-      <div className="StateIssues StatePollValueList">
-        {engine.scenarioController.getIssues().map((issue) => {
+      {showingStateEstimate && <h3>Issue Stances</h3>}
+      
+        {showingStateEstimate ? <div className="StateIssues StatePollValueList">{engine.scenarioController.getIssues().map((issue) => {
           const issueScore = stateController.issueScores.getIssueScoreForIssue(
             issue.id
           );
@@ -65,23 +132,24 @@ function StatePoll(props: StatePollProps) {
               <div className={issue.description == "" ? "IssueDiv" : "IssueDiv Tooltip"} style={{ fontWeight: "bold" }}>{issue.name}{issue.description != "" && <span className="TooltipText">{issue.description}</span>}</div> - {" "} <div className={stanceDescription == "" ? "IssueDiv" : "IssueDiv Tooltip"}>{stance}{stanceDescription != "" && <span className="TooltipText">{stanceDescription}</span>}</div>
             </div>
           );
-        })}
-      </div>
+        })}</div> : evEstimate(engine)}
+      
       <div className="EvPvInfo">
-      {stateController.model.electoralVotes > 0 && <div>Electoral Votes: {numberFormatter.format(stateController.model.electoralVotes)}</div>}
-      {stateController.model.popularVotes > 0 && <div>Popular Votes: {numberFormatter.format(stateController.model.popularVotes)}</div>}
+        {stateController.model.electoralVotes > 0 && <div>Electoral Votes: {numberFormatter.format(stateController.model.electoralVotes)}</div>}
+        {stateController.model.popularVotes > 0 && <div>Popular Votes: {numberFormatter.format(stateController.model.popularVotes)}</div>}
       </div>
       {
         engine.counters.size > 0 &&
         <div className="Counters">
-        <h3>Counters</h3>
+          <h3>Counters</h3>
           {Array.from(engine.counters).map((e) => {
             const key = e[0];
             const value = e[1];
-            return <div>{engine.counterDisplayNames.get(key) ?? key + " - " + value}</div>
+            return <div>{engine.counterDisplayNames.get(key) ?? key + " - " + value}</div>;
           })}
         </div>
       }
+      <button style={{"fontSize":"x-small"}} onClick={() => setShowingStateEstimate(!showingStateEstimate)}>{showingStateEstimate ? "Switch to Overall PV/EV Estimate" : "Switch to State Estimate"}</button>
     </div>
   );
 }
