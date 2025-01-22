7d063a4af91b1c0bfc6260164880576a18f5d6d1
Jet Simon <jetjdsimon@gmail.com>
2025-01-22 07:11:28 -0800

    Update MusicPlayer.tsx

--- open-source-election-game/src/oseg/game/components/MusicPlayer.tsx
+++ open-source-election-game/src/oseg/game/components/MusicPlayer.tsx
@@ -3,112 +3,149 @@ import SongModel from "../../engine/models/SongModel";
 import "./MusicPlayer.css";
 
 interface MusicPlayerProps {
-    songs : SongModel[];
+  songs: SongModel[];
 }
 
-function MusicPlayer(props : MusicPlayerProps) {
+function MusicPlayer(props: MusicPlayerProps) {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const scrubberRef = useRef<HTMLInputElement>(null);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+  const { songs } = props;
+  const [volume, setVolume] = useState(1);
+  const [scrub, setScrub] = useState(0);
 
-    const audioRef = useRef<HTMLAudioElement>(null);
-    const scrubberRef = useRef<HTMLInputElement>(null);
-    const [currentSongIndex, setCurrentSongIndex] = useState(0);
-    const [isPaused, setIsPaused] = useState(false);
-    const { songs } = props;
-    const [volume, setVolume] = useState(1);
-    const [scrub, setScrub] = useState(0);
+  useEffect(() => {
+    if (audioRef == null || audioRef.current == null) {
+      return;
+    }
+  }, [scrub]);
 
-    useEffect(() => {
-        if(audioRef == null || audioRef.current == null) {
-            return;
-        }
-    }, [scrub])
-
-    useEffect(() => {
-        if(audioRef == null || audioRef.current == null) {
-            return;
-        }
-
-        audioRef.current.volume = volume;
-    }, [volume])
-
-    useEffect(() => {
-        if(audioRef == null || audioRef.current == null) {
-            return;
-        }
-
-        if(isPaused) {
-            audioRef.current.pause();
-        }
-        else {
-            audioRef.current.play();
-        }
-        
-    }, [isPaused, audioRef])
-
-    if(songs.length == 0) {
-        return <></>
+  useEffect(() => {
+    if (audioRef == null || audioRef.current == null) {
+      return;
     }
 
-    const currentSong = songs[currentSongIndex];
+    audioRef.current.volume = volume;
+  }, [volume]);
 
-    if(currentSong == null || currentSong.url == null) {
-        return <></>
+  useEffect(() => {
+    if (audioRef == null || audioRef.current == null) {
+      return;
     }
 
-    function changeSongIndex(delta : number) {
-        if(currentSongIndex == 0 && delta < 0) {
-            setCurrentSongIndex(songs.length - 1);
-            return;
-        }
+    if (isPaused) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+  }, [isPaused, audioRef]);
 
-        setCurrentSongIndex((currentSongIndex + delta) % songs.length);
+  if (songs.length == 0) {
+    return <></>;
+  }
+
+  const currentSong = songs[currentSongIndex];
+
+  if (currentSong == null || currentSong.url == null) {
+    return <></>;
+  }
+
+  function changeSongIndex(delta: number) {
+    if (currentSongIndex == 0 && delta < 0) {
+      setCurrentSongIndex(songs.length - 1);
+      return;
     }
 
-    function onEnded() {
-        changeSongIndex(+1);
+    setCurrentSongIndex((currentSongIndex + delta) % songs.length);
+  }
+
+  function onEnded() {
+    changeSongIndex(+1);
+  }
+
+  function scrubTo(t: number) {
+    if (audioRef == null || audioRef.current == null) {
+      return;
     }
 
-    function scrubTo(t : number) {
-        if(audioRef == null || audioRef.current == null) {
-            return;
-        }
+    setScrub(t);
+    audioRef.current.currentTime = audioRef.current.duration * t;
+  }
 
-        setScrub(t);
-        audioRef.current.currentTime = audioRef.current.duration * t;
+  function onTimeUpdated() {
+    if (audioRef == null || audioRef.current == null) {
+      return;
     }
 
-    function onTimeUpdated() {
-        if(audioRef == null || audioRef.current == null) {
-            return;
-        }
-
-        if(document.activeElement == scrubberRef.current) {
-            return;
-        }
-
-        setScrub(audioRef.current.currentTime / audioRef.current.duration);
+    if (document.activeElement == scrubberRef.current) {
+      return;
     }
 
-    return (
-        <div className="MusicPlayer">
-            <audio onTimeUpdate={onTimeUpdated} onEnded={onEnded} loop={songs.length < 2} autoPlay={!isPaused} ref={audioRef} preload="auto" src={currentSong.url}></audio>
-            <img className="MusicPlayerImage" src={currentSong.imageUrl}>
-            </img>
-            <div className="MusicVolumeSliderHolder">
-                <input step="0.05" value={volume} onChange={(e) => setVolume(Number(e.target.value))} min="0" max="1" className="MusicVolumeSlider" type="range"></input>
-                🔊
-            </div>
-            
-            <div className="MusicPlayerMenu">
-                {currentSong.displayName}
-                <div className="MusicPlayerButtons">
-                    <button disabled={songs.length < 2} onClick={() => changeSongIndex(-1)}>⏮</button>
-                    <button onClick={() => setIsPaused(!isPaused)}>{isPaused ? "⏵" : "⏸"}</button>
-                    <button disabled={songs.length < 2} onClick={() => changeSongIndex(+1)}>⏭</button>
-                </div>
-                <input step="0.01" ref={scrubberRef} value={scrub} onChange={(e) => scrubTo(Number(e.target.value))} className="MusicPlayerScrubber" type="range" min="0" max="1"></input>
-            </div>
+    setScrub(audioRef.current.currentTime / audioRef.current.duration);
+  }
+
+  return (
+    <div className="MusicPlayer">
+      <audio
+        onTimeUpdate={onTimeUpdated}
+        onEnded={onEnded}
+        loop={songs.length < 2}
+        autoPlay={!isPaused}
+        ref={audioRef}
+        preload="auto"
+        src={currentSong.url}
+      ></audio>
+      <img className="MusicPlayerImage" src={currentSong.imageUrl}></img>
+      <div className="MusicVolumeSliderHolder">
+        <input
+          step="0.05"
+          value={volume}
+          onChange={(e) =>
+            setVolume(
+              isNaN(Number(e.target.value)) ? 1 : Number(e.target.value)
+            )
+          }
+          min="0"
+          max="1"
+          className="MusicVolumeSlider"
+          type="range"
+        ></input>
+        🔊
+      </div>
+
+      <div className="MusicPlayerMenu">
+        {currentSong.displayName}
+        <div className="MusicPlayerButtons">
+          <button
+            disabled={songs.length < 2}
+            onClick={() => changeSongIndex(-1)}
+          >
+            ⏮
+          </button>
+          <button onClick={() => setIsPaused(!isPaused)}>
+            {isPaused ? "⏵" : "⏸"}
+          </button>
+          <button
+            disabled={songs.length < 2}
+            onClick={() => changeSongIndex(+1)}
+          >
+            ⏭
+          </button>
         </div>
-    );
+        <input
+          step="0.01"
+          ref={scrubberRef}
+          value={scrub}
+          onChange={(e) => scrubTo(Number(e.target.value))}
+          className="MusicPlayerScrubber"
+          type="range"
+          min="0"
+          max="1"
+        ></input>
+      </div>
+    </div>
+  );
 }
 
 export default MusicPlayer;
