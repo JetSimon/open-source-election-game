95351d74faf0050b1f4f10ffd33290b294d870de
damilkz <marcospizzeria45@gmail.com>
2025-01-27 18:36:38 -0500

    Make ev property optional

--- open-source-election-game/src/oseg/engine/models/HistoricalResultsModel.ts
+++ open-source-election-game/src/oseg/engine/models/HistoricalResultsModel.ts
@@ -1,6 +1,6 @@
 interface HistoricalResultsModel {
     popularVotes : Record<number, number>;
-    electoralVotes : Record<number, number>;
+    electoralVotes? : Record<number, number>;
 }
 
 export type { HistoricalResultsModel };
